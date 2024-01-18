11651fdf72eaf09c9b44d36fed5fa4a87192b86a
AdeelSyed897 <82331921+AdeelSyed897@users.noreply.github.com>
2024-01-18 00:29:00 -0600

    NBA.py
    Selenium had a new update and statMuse changed their website slightly so I edited the code to fix those isssues

--- NBA.py
+++ NBA.py
@@ -1,5 +1,7 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -8,8 +10,7 @@ import math
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 
-
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 
 url = 'https://www.statmuse.com/nba'
 
@@ -20,48 +21,50 @@ print('\n')
 name = input('What Player: ')
 print('\n\n')
 
-driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[2]/div/ask-bar/form/div/div[1]/textarea').send_keys(name + " 2022-2023 regular season home stats") #can be home/away to compare those stats
-driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[2]/div/ask-bar/form/div/div[1]/input').click()
+driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[3]/div/astro-island/form/div[2]/div[1]/textarea').send_keys(name + " home stats regular season") 
+driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[3]/div/astro-island/form/div[2]/div[1]/input').click()
+time.sleep(3)
 page = driver.page_source
 soup = BeautifulSoup(page, 'html.parser')
-
-
-stats = soup.find_all(attrs={"class": "text-sm font-semibold"})
+stats = soup.find_all(attrs={"class": "grid grid-cols-[1fr_min(1250px,_100%)_1fr] w-full px-3 md:px-[75px] [&>*]:col-span-1 [&>*]:col-start-2"})
 
 
 for stat in stats:
     row_content = stat.find_all("td")
-    hPPG = float(row_content[8].text.strip())
-    hReb = float(row_content[9].text.strip())
-    hAst = float(row_content[10].text.strip())
-    hStl = float(row_content[11].text.strip())
-    hBlk = float(row_content[12].text.strip())
-    hTOV = float(row_content[13].text.strip())
-    hFG = float(row_content[16].text.strip())
+    hPPG = float(row_content[4].text.strip())
+    hReb = float(row_content[5].text.strip())
+    hAst = float(row_content[6].text.strip())
+    hStl = float(row_content[7].text.strip())
+    hBlk = float(row_content[8].text.strip())
+    hTOV = float(row_content[9].text.strip())
+    hFG = float(row_content[12].text.strip())
     break
 
 
-driver.find_element(By.XPATH,'/html/body/div[2]/div[1]/a').click()
-
-driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[2]/div/ask-bar/form/div/div[1]/textarea').send_keys(name + " 2022-2023 regular season away stats") #can be home/away to compare those stats
-driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[2]/div/ask-bar/form/div/div[1]/input').click()
+driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/a').click()
+time.sleep(3)
+driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[3]/div/astro-island/form/div[2]/div[1]/textarea').send_keys(name + " away stats regular season") 
+driver.find_element(By.XPATH,'//*[@id="home"]/div[3]/div[3]/div/astro-island/form/div[2]/div[1]/input').click()
+time.sleep(3)
 page = driver.page_source
 soup = BeautifulSoup(page, 'html.parser')
+stats = soup.find_all(attrs={"class": "grid grid-cols-[1fr_min(1250px,_100%)_1fr] w-full px-3 md:px-[75px] [&>*]:col-span-1 [&>*]:col-start-2"})
 
-
-stats = soup.find_all(attrs={"class": "text-sm font-semibold"})
 
 
 for stat in stats:
     row_content = stat.find_all("td")
-    aPPG = float(row_content[8].text.strip())
-    aReb = float(row_content[9].text.strip())
-    aAst = float(row_content[10].text.strip())
-    aStl = float(row_content[11].text.strip())
-    aBlk = float(row_content[12].text.strip())
-    aTOV = float(row_content[13].text.strip())
-    aFG = float(row_content[16].text.strip())
+    aPPG = float(row_content[4].text.strip())
+    aReb = float(row_content[5].text.strip())
+    aAst = float(row_content[6].text.strip())
+    aStl = float(row_content[7].text.strip())
+    aBlk = float(row_content[8].text.strip())
+    aTOV = float(row_content[9].text.strip())
+    aFG = float(row_content[12].text.strip())
     break
+
+print('Home Statistics:',hPPG,' PPG ', hReb,' RPG ', hAst,' APG ', hStl,' SPG ', hBlk,' BPG ', hTOV,' TPG ', hFG,' FG% ')
+print('Away Statistics:',aPPG,' PPG ', aReb,' RPG ', aAst,' APG ', aStl,' SPG ', aBlk,' BPG ', aTOV,' TPG ', aFG,' FG% ')
 
 PPG = ((aPPG-hPPG)/hPPG)*100
 PPG = math.ceil(PPG*100)/100
@@ -84,7 +87,6 @@ TOV = math.ceil(TOV*100)/100
 FG = ((aFG-hFG)/hFG)*100
 FG = math.ceil(FG*100)/100
 
-print(name,'had a\n',PPG,'percent change in PPG\n',REB,'percent change in Rebounds\n',AST,'percent change in Assits\n',STL,'percent change in Steals\n',BLK,'percent change in Blocks\n',TOV,'percent change in Turn Overs\n',FG,'percent change in Field Goal Percentage\n')
+print(name,'averaged a\n',PPG,'percent change in PPG\n',REB,'percent change in Rebounds\n',AST,'percent change in Assits\n',STL,'percent change in Steals\n',BLK,'percent change in Blocks\n',TOV,'percent change in Turn Overs\n',FG,'percent change in Field Goal Percentage\n')
 
 print('\n\n\n')
-
